bc81d6f9902278428743f53a9b1a4046545b36ca
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2022-08-29 16:54:00 +0200

    replace in base64 strings (#9)

--- src/Replacer/Substitution/ISecretReplacer.cs
+++ src/Replacer/Substitution/ISecretReplacer.cs
@@ -1,5 +1,6 @@
 ﻿namespace Replacer.Substitution;
 
+using System.Text;
 using System.Text.RegularExpressions;
 using SecretsProvider;
 
@@ -11,16 +12,24 @@ public interface ISecretReplacer
 public class SecretReplacer : ISecretReplacer
 {
     private const string Pattern = @"<[ \t]*(?<store>secret|sops):(?<path>[^\r\n\|]*)\|?(?<modifiers>[^\r\n]*)>";
+    private const string Base64Pattern = @"[A-Za-z0-9\+\/\=]{10,}";
     private readonly Regex regexKey = new(Pattern, RegexOptions.Singleline | RegexOptions.Compiled);
+    private readonly Regex regexBase64 = new(Base64Pattern, RegexOptions.Singleline | RegexOptions.Compiled);
     private readonly ISecretsProvider secretsProvider;
 
     public SecretReplacer(ISecretsProvider secretsProvider)
         => this.secretsProvider = secretsProvider ?? throw new ArgumentNullException(nameof(secretsProvider));
 
     public string Replace(string line)
-        => regexKey.Replace(line, match => Evaluator(match, secretsProvider));
+        => ReplaceKey(ReplaceBase64(line));
 
-    private static string Evaluator(Match match, ISecretsProvider secretsProvider)
+    private string ReplaceBase64(string line)
+        => regexBase64.Replace(line, Base64Evaluator);
+
+    private string ReplaceKey(string line)
+        => regexKey.Replace(line, Evaluator);
+
+    private string Evaluator(Match match)
     {
         var path = match.Groups["path"].Value;
         var modifiers = match.Groups["modifiers"].Value.Split('|');
@@ -28,5 +37,44 @@ public class SecretReplacer : ISecretReplacer
         return match.Success
             ? secret
             : match.Value;
+    }
+
+    private string Base64Evaluator(Group match)
+    {
+        if (!match.Success)
+        {
+            return match.Value;
+        }
+
+        var (decodedString, error) = FromBase64(match.Value);
+        if (error)
+        {
+            return match.Value;
+        }
+
+        var replaceResult = ReplaceKey(decodedString);
+        return replaceResult == decodedString
+            ? match.Value
+            : ToBase64(replaceResult);
+    }
+
+    private static (string value, bool error) FromBase64(string data)
+    {
+        try
+        {
+            var bytes = Convert.FromBase64String(data);
+            var value = Encoding.UTF8.GetString(bytes);
+            return (value, false);
+        }
+        catch (FormatException)
+        {
+            return (data, true);
+        }
+    }
+
+    private static string ToBase64(string data)
+    {
+        var bytes = Encoding.UTF8.GetBytes(data);
+        return Convert.ToBase64String(bytes);
     }
 }
